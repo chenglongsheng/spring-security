b80be072217cce790e129b71403a4675b664c9b6
成龙生 <1536463948@qq.com>
2022-09-15 20:33:45 +0800

    测试请求规则

---
+++ demo2/src/main/java/cn/cls/demo2/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package cn.cls.demo2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .mvcMatchers("/user/login").permitAll()
+                .anyRequest().denyAll();
+        return http.build();
+    }
+
+}

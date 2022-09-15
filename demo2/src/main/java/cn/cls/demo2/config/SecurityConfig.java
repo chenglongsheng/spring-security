c3fd9b08bdcf21b60f8ef3f447cda2db93a7bf45
成龙生 <1536463948@qq.com>
2022-09-15 23:39:00 +0800

    使用ProviderManager auth方法进行验证

--- demo2/src/main/java/cn/cls/demo2/config/SecurityConfig.java
+++ demo2/src/main/java/cn/cls/demo2/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package cn.cls.demo2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -25,6 +32,14 @@ public class SecurityConfig {
                 .mvcMatchers("/user/login").permitAll()
                 .anyRequest().denyAll();
         return http.build();
+    }
+
+    /**
+     * 使用ProviderManager auth方法进行验证
+     */
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }

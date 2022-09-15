1233b578420acc34a2ac2a660b9bb7438dc4ae47
成龙生 <1536463948@qq.com>
2022-09-15 11:19:48 +0800

    配置认证过滤器，创建认证管理器

--- security-demo/src/main/java/cn/cls/securitydemo/config/SecurityConfig.java
+++ security-demo/src/main/java/cn/cls/securitydemo/config/SecurityConfig.java
@@ -1,37 +1,46 @@
 package cn.cls.securitydemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().antMatchers("/login").permitAll()
-//                .antMatchers("/users/**" , "/settings/**")
-//                .hasAuthority("Admin")
-//                .hasAnyAuthority("Admin" , "Editor" , "Salesperson")
-//                .hasAnyAuthority("Admin" , "Editor" , "Salesperson" , "Shipper")
-//                .anyRequest().authenticated()
-//                .and().formLogin()
-//                .loginPage("/login")
-//                .usernameParameter("email")
-//                .permitAll()
-//                .and()
-//                .rememberMe().key("AbcdEfghIjklmNopQrsTuvXyz_0123456789")
-//                .and()
-//                .logout().permitAll();
-//
-//        http.headers().frameOptions().sameOrigin();
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // 关闭csrf
+                .csrf().disable()
+                // 不通过Session获取SecurityContext
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // 对于登录接口 允许匿名访问
+                .antMatchers("/user/login").anonymous()
+                // 除上面外的所有请求全部需要鉴权认证
+                .anyRequest().authenticated();
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }

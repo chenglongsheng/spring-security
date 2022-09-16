576ba92447c98a5fe2bfd95d47033e7dcbc281a1
成龙生 <1536463948@qq.com>
2022-09-16 11:26:00 +0800

    测试加密

--- security/src/test/java/cn/cls/security/SecurityApplicationTests.java
+++ security/src/test/java/cn/cls/security/SecurityApplicationTests.java
@@ -5,6 +5,7 @@ import cn.cls.security.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -13,6 +14,17 @@ class SecurityApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void testBCryptPasswordEncoder() {
+        String encode = passwordEncoder.encode("123");
+        String encode2 = passwordEncoder.encode("123");
+        System.out.println(encode);
+        System.out.println(encode2);
+    }
 
     @Test
     void testMybatisPlus() {
